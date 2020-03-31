7233d8671ac024f459d628d9ed6c531da58f8f2a
cryptozik <cryptozik@hodl.farm>
2020-04-01 01:51:58 +0800

    Improved payout.ms template to outout payout details within the source table.

--- docker/website-builder/createPayoutPages.py
+++ docker/website-builder/createPayoutPages.py
@@ -1,47 +1,36 @@
-# coding=utf-8
-
 import json 
 import sys
+import os
+from jinja2 import Template
+
+# 6 - offset to calculate the cycle when the actual payout is processed
+payout_offset = int(os.environ["PAYOUT_DELAY"]) + 6 
 
 save_path = sys.argv[1]
 
-with open('payouts.json') as json_file:
+with open('Payouts.json') as json_file:
     raw_payouts = json.load(json_file)
-delegators = {}
-for cycle,cycle_val in raw_payouts["payoutsByCycle"].items():
-    for delegator in cycle_val["delegators"]:
-        if not delegator in delegators:
-            delegators[delegator] = ["---"]
-            delegators[delegator].append("layout: about")
-            delegators[delegator].append("---")
-            delegators[delegator].append("### Payout to address [%s](https://tzstats.com/%s){:target=\"_blank\"}:" % (delegator, delegator))
-            delegators[delegator].append("")
-            delegators[delegator].append("|Cycle|Balance|Payout|Actual fee|Payout operation|")
-            delegators[delegator].append("|-----|-------|------|----------|----------------|")
-        delegator_val=cycle_val["delegators"][delegator]
-        if "payoutOperationHash" in delegator_val:
-            payout_operation = "[%s...](https://tzstats.com/%s)" % ( delegator_val["payoutOperationHash"][0:7], delegator_val["payoutOperationHash"]) 
-        else:
-            payout_operation = ""
-        if delegator_val["estimatedRewards"] != "0":
-            delegators[delegator].append("|%s|%sꜩ|%sꜩ|%s|%s|" % (cycle, int(delegator_val["balance"]) / 1000000,
-            int(delegator_val["estimatedRewards"]) / 1000000,
-            "%s%%" % round( ( 1 - ( 0.95 * int(delegator_val["estimatedRewards"]) / int(delegator_val["finalRewards"]) ) ) * 100, 3) if "finalRewards" in delegator_val else "Not yet known",
-            payout_operation ) )
 
-for delegator, delegator_val in delegators.items():
-    delegator_val.append("")
-    delegator_val.append("[How do payouts work ?](https://hodl.farm/faq.html#how-do-payouts-work-)")
-    print("\n".join(delegator_val), file=open("%s/%s.md" % ( save_path, delegator), "a"))
+#Crreate Payouts by delegator dictionary
+payout_by_delegator={}
+for cycle_id,cycle_details in raw_payouts["payoutsByCycle"].items():
+    for delegator_id,delegator_details in cycle_details["delegators"].items():
+        if delegator_id not in payout_by_delegator:
+            payout_by_delegator[delegator_id]={}
+        delegator_details["paid_in_cycle"]= int(cycle_id)+payout_offset
+        delegator_details["balance"]= int(delegator_details["balance"])/1000000
+        if "payoutAmount" in delegator_details:
+            delegator_details["payoutAmount"]= int(delegator_details["payoutAmount"])/1000000
+        else: 
+            #the case statement below accomodates legacy data structure before cycle 210
+            delegator_details["payoutAmount"]= int(delegator_details["estimatedRewards"])/1000000
+        if "payoutWithheldDebt" in delegator_details:
+            delegator_details["payoutWithheldDebt"]= -int(delegator_details["payoutWithheldDebt"])/1000000
+        delegator_details["estimatedRewards"]= int(delegator_details["estimatedRewards"])/1000000
+        payout_by_delegator[delegator_id][cycle_id]=delegator_details
 
-# Explanation of "actual fee"
-# How to calculate the actual fee with the estimated rewards and actual rewards:
-# An example when the nominal fee is 5%
-# Your estimated rewards (that we pay you) is x
-# Your share of our idealized earnings is x/(1-fee) i.e x/0.95
-# The actual rewards we should have paid you is y
-# Your share of actual earnings is y/0.95
-# The effective fee is  ( y/0.95 - x ) / ( y/0.95) = 1 - 0.95 ( x / y )
-# We can confirm this calculation with the following hypothesis:
-# Let's say the network behaved optimally. In that case, y = x
-# Then the effective fee is 1 - 0.95 = 0.05, which is correct
+for delegator_id,delegator_details in payout_by_delegator.items():   
+    template = Template(open('payouts.md').read()) 
+    output = template.render(delegator_id = delegator_id,delegator_details=delegator_details)
+    with open("%s/%s.md" % ( save_path, delegator_id), "w") as f:
+        f.write(output)
--- docker/website-builder/payouts.md
+++ docker/website-builder/payouts.md
@@ -2,6 +2,35 @@
 layout: about
 ---
 ### Payout to address [{{ delegator_id }}](https://tzstats.com/{{ delegator_id }}){:target="_blank"}:
+<style>
+table.nested_table {
+border-collapse: collapse;
+margin-bottom: 0px;
+border: 0px;
+}
+table td.nested_td {
+border: 0px;
+text-align: right;
+margin-bottom: 1px;
+padding: 0px 15px;
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+function expand_details(cycle_id) 
+{   
+    if ($("#caret_"+cycle_id).hasClass("fa-caret-right")){
+        $(".row_details").hide();
+        $(".cycle_arrow").removeClass("fa-caret-down").addClass("fa-caret-right");
+        $("#row_details_"+cycle_id).show();
+        $("#caret_"+cycle_id).removeClass("fa-caret-right").addClass("fa-caret-down");
+    }
+    else {
+        $("#row_details_"+cycle_id).hide();
+        $("#caret_"+cycle_id).removeClass("fa-caret-down").addClass("fa-caret-right");
+    }    
+}
+</script>
 
 <table class="{{tableclass}}" id="delegators">
     <thead>
@@ -14,21 +43,36 @@ layout: about
         {%- for cycle_id,delegator_details_for_cycle in delegator_details.items() %}
            <tr>
                 <td>{{ cycle_id }}</td>
-                <td>ꜩ{{ delegator_details_for_cycle[("balance")] }}</td>          
+                <td>ꜩ{{ delegator_details_for_cycle[("balance")] }}</td>        
                 <td>ꜩ{{ delegator_details_for_cycle[("payoutAmount")] }}</td> 
-                <td><a href="#">details</a></td>
+                <td>
+					<label for="details" onclick="expand_details({{cycle_id}})">View details</label>
+                    <i id="caret_{{cycle_id}}" class="fa fa-caret-right cycle_arrow" onclick="expand_details({{cycle_id}})"></i>
+				</td>
            </tr>
-           <tr>
-                <td colspan="4"><b>Payout Cycle details</b> 
-                    <br/>Estimated reward for {{ cycle_id }}, ꜩ: {{delegator_details_for_cycle[("estimatedRewards")] }}
-                {% if delegator_details_for_cycle[("payoutWithheldDebt")] %}
-                    <br/>Debt amount for cycle {{ delegator_details_for_cycle[("withheldDebtForCycle")] }}, ꜩ: {{delegator_details_for_cycle[("payoutWithheldDebt")] }}
-                {% endif %}
-                    <br/>Final payout paid in cycle {{ delegator_details_for_cycle[("paid_in_cycle")] }}, ꜩ: {{delegator_details_for_cycle[("payoutAmount")] }}
-                    </td>
+           <tr id="row_details_{{cycle_id}}" class="row_details" style="display:none">
+                <td class="delegator_details" colspan="4">
+                    <table class="nested_table" >
+                            <tr>
+                                <td class="nested_td">Estimated reward for cycle {{ cycle_id }}, ꜩ: </td> 
+                                <td class="nested_td">{{ delegator_details_for_cycle[("estimatedRewards")] }}</td>
+                            </tr>
+                            {% if delegator_details_for_cycle[("payoutWithheldDebt")] %}
+                            <tr>
+                                <td class="nested_td">Debt amount for cycle {{ delegator_details_for_cycle[("withheldDebtForCycle")] }}, ꜩ:</td>
+                                <td class="nested_td">{{ delegator_details_for_cycle[("payoutWithheldDebt")] }}</td> 
+                            </tr>
+                            {% endif %}
+                            <tr>
+                                <td class="nested_td">Total paid in cycle {{ delegator_details_for_cycle[("paid_in_cycle")] }}, ꜩ:</td>
+                                <td class="nested_td">{{delegator_details_for_cycle[("payoutAmount")] }}</td> 
+                            </tr>
+                    </table>
+                </td>
             </tr>
         {%- endfor %}
-        </tbody>
+    </tbody>
 </table>
 
 [How do payouts work ?](https://hodl.farm/faq.html#how-do-payouts-work-)
+
